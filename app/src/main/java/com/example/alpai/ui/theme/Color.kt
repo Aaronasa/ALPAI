47b59dd0fc99ea89e9083e5b9f38359e22283bed
Ueno <153161447+Uenoooooo@users.noreply.github.com>
2026-01-08 20:34:25 +0700

    update color

--- app/src/main/java/com/example/alpai/ui/theme/Color.kt
+++ app/src/main/java/com/example/alpai/ui/theme/Color.kt
@@ -2,10 +2,11 @@ package com.example.alpai.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val GreenPrimary = Color(0xFF2D6A4F)
+val GreenSecondary = Color(0xFF52B788)
+val GreenTertiary = Color(0xFF95D5B2)
+val GreenBackground = Color(0xFFF8FBF9)
+
+val DarkGrey = Color(0xFF1A1C1A)
+val SoftWhite = Color(0xFFFFFFFF)
